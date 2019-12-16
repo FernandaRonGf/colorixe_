c65969dc63dc705183aff0415feba0432129c29b
FernandaRonGf <fernanda@gafa.mx>
2019-12-16 12:41:48 -0600

    [add] components / react-color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ColorCard from './components/ColorCard';
+import Header from './components/Header'
+import ImageInput from './components/ImageInput'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Header />
+  <ColorCard />
+  <ImageInput/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+ 
+
+  render() {
+    return (
+        <div>
+           <h3>
+               Colorixe _ /
+           </h3>
+        </div>
+    );
+  }
+}
+export default Header;
